af6cc2bc1f7c73e3d2aa084de0139866e1ab9d18
reedhong <reedhong@163.com>
2019-08-13 01:34:30 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ERC20 token for Cocos-BCX
+# ERC20 for Cocos-BCX 
 
+## 执行步骤：
+1. 创建合约 CocosToken
+
+2. 创建合约 CocosTokenLock
+*     设置正确的间隔时间
+*     设置正确的钱包账号
+
+3. 设置创建合约CocosToken的账号为白名单
+
+4. 设置合约 CocosTokenLock 的地址为白名单
+
+5. 把创建合约CocosToken的账号身上的token全部转给 CocosTokenLock 合约地址
+
+6. 调用 CocosTokenLock 的 lock接口，锁住所有的token
+
+7. 以此调用 CocosTokenLock 的 unlock 接口传入不同的类型，解锁token
+
+8. 设置 CocosToken 为 unpuased
--- flatten_contracts/CocosTokenLockFlat.sol
+++ flatten_contracts/CocosTokenLockFlat.sol
@@ -506,6 +506,7 @@ pragma solidity ^0.5.0;
 /**
  * @title CocosTokenLock
  * @dev CocosToken for lock some Cocos token
+ * @author reedhong
  */
 contract CocosTokenLock is Ownable {
     using SafeMath for uint;
@@ -519,42 +520,60 @@ contract CocosTokenLock is Ownable {
     // 4 PartnerIncentive
     // 5 DPOSReward
     // 6 TokenTreasury
-    uint8 public constant CONTRIBUTOR = 0;
-    uint8 public constant TEAM = 1;
-    uint8 public constant ADIVISORS = 2;
-    uint8 public constant USER_INCENTIVE = 3;
-    uint8 public constant PARTNER_INCENTIE = 4;
-    uint8 public constant DPOS_REWARD = 5;
-    uint8 public constant TOKEN_TREASURY = 6;
-    uint8 public constant TOTAL_LOCK_TYPE = 7;
+    uint8 public constant CONTRIBUTOR = 0;  // 4
+    uint8 public constant TEAM = 1; // 3
+    uint8 public constant ADIVISORS = 2; // 13
+    uint8 public constant USER_INCENTIVE = 3; // 41
+    uint8 public constant PARTNER_INCENTIE = 4; // 21
+    uint8 public constant DPOS_REWARD = 5; // 120
+    uint8 public constant TOKEN_TREASURY = 6; //21
+    uint8 public constant TOTAL_LOCK_TYPES = 7;
 
-    uint256 public constant DECIMAL_VALUE = (10 ** 18);
+    uint256 public constant DV = (10 ** 18);
 
 
-    uint[] public lockPerK = [224, 170, 40, 76, 100, 300, 90];
-    //uint[] public startTimes = [0, 120 days, 0, 0, 0, 90 days, 0];  // from tge(token generator event)
-    uint[] public startTimes = [0, 5 minutes, 0, 0, 0, 6 minutes, 0];  // for test
-    //uint[] public locakIntervalTimes = [180 days, 365 days, 180 days, 90 days, 90 days, 30 days, 90 days];
-    uint[] public locakIntervalTimes = [2 minutes, 3 minutes, 4 minutes, 5 minutes, 6 minutes, 7 minutes, 8 minutes]; // for tet
-    // serven token address
-    address[] public tokenAddresses = [
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1,
-        0x00b9070E042fc812047B00aC41c0D3C631Ba06b1
+    uint256[TOTAL_LOCK_TYPES] public tokenDistribution = [
+        22396452170 * DV,
+        17000000000 * DV,
+        4000000000 * DV,
+        7603547830 * DV,
+        10000000000 * DV,
+        30000000000 * DV,
+        9000000000 * DV
     ];
 
-    uint256[TOTAL_LOCK_TYPE][] public lockTokenMatrix = new uint256[TOTAL_LOCK_TYPE][](0);
 
-    uint[] public lastUnlockTimes= [0, 0, 0, 0, 0, 0, 0];
-    uint[] public currentLockSteps = [0, 0, 0, 0, 0, 0, 0];
+    //uint[TOTAL_LOCK_TYPES] public startTimes = [0, 120 days, 30 days, 0, 0, 90 days, 0];  // from tge(token generator event)
+    uint[TOTAL_LOCK_TYPES] public startTimes = [0,  10 seconds, 1 minutes, 0 minutes, 0, 10 seconds, 0];  // for test
+    //uint[TOTAL_LOCK_TYPES] public lockIntervalTimes = [180 days, 365 days, 90 days, 90 days, 90 days, 30 days, 90 days];
+    uint[TOTAL_LOCK_TYPES] public lockIntervalTimes = [10 seconds, 10 seconds,  3 minutes, 5 seconds,
+        3 minutes, 2 seconds, 5 seconds];  // for test
+
+    //uint public constant ADIVISORS_SECOND_AHEAD_TIME = 30 days;
+    uint public constant ADIVISORS_SECOND_AHEAD_TIME = 2 minutes;  // for test
+    //uint public constant PARTNER_INCENTIE_SECOND_DELAY_TIME = 30 days;
+    uint public constant PARTNER_INCENTIE_SECOND_DELAY_TIME = 2 minutes; // for test
+
+    // seven token address
+    address[TOTAL_LOCK_TYPES] public tokenAddresses = [
+        0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C,
+        0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB,
+        0x583031D1113aD414F02576BD6afaBfb302140225,
+        0xdD870fA1b7C4700F2BD7f44238821C26f7392148,
+        0x67031f5C338a1e27A1E5B8Ff9bbbA8ab5C59DDC0,
+        0x44fAA078bd37caD14238a1A4b267E33E9AACf11d,
+        0x6CAb2F342BdD4E5657811f19d639F42A7343bDE8
+    ];
+
+    uint256[][TOTAL_LOCK_TYPES] public lockTokenMatrix;
+
+    uint[TOTAL_LOCK_TYPES] public lastUnlockTimes= [0, 0, 0, 0, 0, 0, 0];
+    uint[TOTAL_LOCK_TYPES] public currentLockSteps = [0, 0, 0, 0, 0, 0, 0];
 
     uint public lockedAt = 0;
 
     event UnlockToken(uint8 tokenType, uint currentStep, uint steps, uint256 tokens, uint lockTime, address addr);
+    event CheckTokenDistribution(uint tokenType, uint256 distribution);
 
     //Has not been locked yet
     modifier notLocked {
@@ -573,17 +592,74 @@ contract CocosTokenLock is Ownable {
         _;
     }
 
-    constructor(address payable _cocosToken, address _privateLockAddress)
-        public
-        validAddress(_cocosToken)
-        validAddress(_privateLockAddress){
+    modifier validTokenType(uint8 tokenType ){
+        require(0 <= tokenType && tokenType < TOTAL_LOCK_TYPES, "tokenType must in [0,6]");
+        _;
+    }
 
+    constructor(address payable _cocosToken)
+        public
+        validAddress(_cocosToken){
         token = ERC20(_cocosToken);
 
-        lockTokenMatrix[CONTRIBUTOR] = new uint256[3];
+        lockTokenMatrix[CONTRIBUTOR] = [
+            12025214795 * DV,
+            5353626719 * DV,
+            4582828047 * DV,
+            434782609 * DV
+        ];
+        lockTokenMatrix[TEAM] = [
+            5692583331 * DV,
+            5691833331 * DV,
+            5615583338 * DV
+        ];
+        lockTokenMatrix[ADIVISORS] = [
+            350000000 * DV,
+            433333332 * DV,
+            577833332 * DV,
+            83333332 * DV,
+            577833332 * DV,
+            83333332 * DV,
+            577833332 * DV,
+            83333332 * DV,
+            899833332 * DV,
+            83333332 * DV,
+            83333332 * DV,
+            83333332 * DV,
+            83333348 * DV
+        ];
 
-        // add check logic
+        lockTokenMatrix[USER_INCENTIVE] = new uint256[](41);
+        for(uint256 i = 0; i < 40; i++){
+            lockTokenMatrix[USER_INCENTIVE][i] = 190000000 * DV;
+        }
+        lockTokenMatrix[USER_INCENTIVE][40] = 3547830 * DV; //totalSupply * lockPerK[USER_INCENTIVE] - 190000000 * DV * 40;
+
+        lockTokenMatrix[PARTNER_INCENTIE] = new uint256[](21);
+        lockTokenMatrix[PARTNER_INCENTIE][0] = 2000000000 * DV;
+        for(uint256 i = 1; i < 21; i++){
+            lockTokenMatrix[PARTNER_INCENTIE][i] = 400000000 * DV;
+        }
+
+        lockTokenMatrix[DPOS_REWARD] = new uint256[](120);
+        for(uint256 i = 0; i < 120; i++){
+            lockTokenMatrix[DPOS_REWARD][i] = 250000000 * DV;
+        }
+
+        lockTokenMatrix[TOKEN_TREASURY] = new uint256[](21);
+        lockTokenMatrix[TOKEN_TREASURY][0] = 1500000000 * DV;
+        for(uint256 i = 1; i < 21; i++){
+            lockTokenMatrix[TOKEN_TREASURY][i] = 375000000 * DV;
+        }
     }
+
+    function setAddress(uint8 tokenType, address addr) public
+        onlyOwner
+        validAddress(addr)
+        validTokenType(tokenType) {
+            tokenAddresses[tokenType] = addr;
+        }
+
 
     //In the case locking failed, then allow the owner to reclaim the tokens on the contract.
     //Recover Tokens in case incorrect amount was sent to contract.
@@ -591,37 +667,60 @@ contract CocosTokenLock is Ownable {
         notLocked
         onlyOwner{
         // Transfer all tokens on this contract back to the owner
-        require(token.transfer(owner(), token.balanceOf(address(this))), "transfoer error");
+        require(token.transfer(owner(), token.balanceOf(address(this))), "transfer error");
     }
 
     function lock() public
         notLocked
         onlyOwner{
-        //require(token.balanceOf(address(this)) == totalLockToken, );
-        // // add check logic
+        uint256 totalSupply = token.totalSupply();
+        require(token.balanceOf(address(this)) == totalSupply, "can't enough token");
+
+        // check lock percentage
+        uint tokenCount = 0;
+        for(uint i = 0; i < tokenDistribution.length; i++){
+            tokenCount = tokenCount + tokenDistribution[i];
+        }
+        require(tokenCount == totalSupply, "error lock rate, please check it again");
+
+        // check token set
+        for(uint i = 0; i < TOTAL_LOCK_TYPES; i++){
+            uint256 tokens = tokenDistribution[i];
+            uint256 count = 0;
+            for(uint j = 0; j < lockTokenMatrix[i].length; j++ ){
+                count = count + lockTokenMatrix[i][j];
+            }
+            require(tokens == count, "error token set");
+            emit CheckTokenDistribution(i, tokens);
+        }
+
         lockedAt = block.timestamp;
     }
 
     /**
-    * @notice Transfers tokens held by timelock to private.
+    * @notice unlock token by type
     */
     function unlock(uint8 tokenType) public
         locked
-        onlyOwner{
-
-        require(0 <= tokenType && tokenType < TOTAL_LOCK_TYPE, "tokenType must in [0,6]");
+        onlyOwner
+        validTokenType(tokenType){
         require(currentLockSteps[tokenType] < lockTokenMatrix[tokenType].length, "unlock finish");
 
         uint currentTime = block.timestamp;
 
-        uint dt;
-        if(lastUnlockTimes[tokenType] == 0){
-            dt = currentTime - lockedAt;
+        uint steps;
+        bool isFirst = false;
+        if(lastUnlockTimes[tokenType] == 0){  // first time
+            uint interval = currentTime - lockedAt;
+            if( interval > startTimes[tokenType]){
+                steps = 1;
+                isFirst = true;
+            }
         }else{
-            dt = currentTime - lastUnlockTimes[tokenType];
+            uint dt = currentTime - lastUnlockTimes[tokenType];
+            steps = dt/lockIntervalTimes[tokenType];
         }
 
-        uint steps = dt/locakIntervalTimes[tokenType];
         require(steps > 0, "can't unlock");
 
         uint256 unlockToken = 0;
@@ -631,6 +730,18 @@ contract CocosTokenLock is Ownable {
             unlockToken = unlockToken + lockTokenMatrix[tokenType][i];
         }
         lastUnlockTimes[tokenType] = currentTime;
+ 
+        // first time is different
+        if(isFirst){
+            if( tokenType == ADIVISORS){
+                // speed one month
+                lastUnlockTimes[tokenType] = lastUnlockTimes[tokenType] - ADIVISORS_SECOND_AHEAD_TIME;
+            }else if(tokenType == PARTNER_INCENTIE ) {
+                // slow one month
+                lastUnlockTimes[tokenType] = lastUnlockTimes[tokenType] + PARTNER_INCENTIE_SECOND_DELAY_TIME;
+            }
+        }
+
         currentLockSteps[tokenType] = currentLockSteps[tokenType] + steps;
 
         uint256 amount = token.balanceOf(address(this));
